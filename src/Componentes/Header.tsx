8de12e914c6af564a2d25c6057a4cf2c153436ea
D240021 <danielbarrantesq@gmail.com>
2025-05-03 23:57:33 -0600

    se corrije rutas de imagenes

--- src/Componentes/Header.tsx
+++ src/Componentes/Header.tsx
@@ -21,14 +21,14 @@ export default function Header({ carrito, eliminarDeCarrito, incrementarCantidad
                 <div className="row justify-content-center justify-content-md-between">
                     <div className="col-8 col-md-3">
                         <a href="index.html">
-                            <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
+                            <img className="img-fluid" src="./img/logo.svg" alt="imagen logo" />
                         </a>
                     </div>
                     <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
                         <div className="carrito">
                             <img
                                 className="img-fluid"
-                                src="/img/carrito.png"
+                                src="./img/carrito.png"
                                 alt="imagen carrito"
                             />
 
@@ -53,7 +53,7 @@ export default function Header({ carrito, eliminarDeCarrito, incrementarCantidad
                                                 {carrito.map((guitarra) => (
                                                     <tr key={guitarra.id}>
                                                         <td>
-                                                            <img className="img-fluid" src={`/img/${guitarra.image}.jpg`} alt="imagen guitarra" />
+                                                            <img className="img-fluid" src={`./img/${guitarra.image}.jpg`} alt="imagen guitarra" />
                                                         </td>
                                                         <td>{guitarra.name}</td>
                                                         <td className="fw-bold">${guitarra.price}</td>
